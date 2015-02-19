8016c021acf83e0a14c5858a7e012de539687a86
Administrador <Administrador@LENOVO-842AF4C6>
2015-02-19 09:27:42 +0100

    Commit 2

--- src/COD/EntradaDatos.java
+++ src/COD/EntradaDatos.java
@@ -10,16 +10,19 @@ import javax.swing.JOptionPane;
  * @author Administrador
  */
 public class EntradaDatos {
-    public static int introducir(int tipo){
+    public static int introducir(int tipo) throws Exception{
         Scanner dato=new Scanner(System.in);
         int num=0;
         if (tipo==0){
-         num=Integer.parseInt(JOptionPane.showInputDialog("Introduce un número", null));
+           num=Integer.parseInt(JOptionPane.showInputDialog("Introduce un número", null));
         }
         else if (tipo==1){
-         System.out.println("Introduce un número");
-         num=dato.nextInt();
+           System.out.println("Introduce un número");
+           num=dato.nextInt();
         }
+        else 
+            throw new Exception("Error interno, introduce un 0 o 1 en el metodo");
+       
         return num;
     }
 }
